e589e483902176bf17de90d91f4f2dc15b1f73a5
johannes545 <Johannes545@gmail.com>
2022-02-14 22:35:25 +0100

    while loop

---
+++ random/random/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace random
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 10);
+            int userInput = 0;
+
+
+            
+
+            while (favNumber != userInput) 
+            {
+                Console.WriteLine("what is your favorite number?");
+                userInput = int.Parse(Console.ReadLine());
+
+                
+                if (favNumber > userInput)
+                {
+                    Console.WriteLine("too low");
+                    
+                }
+                else if (favNumber < userInput)
+                {
+                    Console.WriteLine("too high");
+                }
+                else
+                {
+                    Console.WriteLine("spot on, congratz");
+                }
+
+            }
+            
+            
+
+        }
+    }
+}
