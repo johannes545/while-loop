df37b82d546dd2bbdcb03bca8dd48ba336c956c9
johannes545 <Johannes545@gmail.com>
2022-02-15 16:25:54 +0100

    both excercises done

---
+++ excercise 2/excercise 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace excercise_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("what is your favorite school subject?");
+            var favoriteSubject = Console.ReadLine();
+            switch (favoriteSubject)
+            {
+                case "Math":
+                    Console.WriteLine("wow your a real chad");
+                    break;
+                case "Science":
+                    Console.WriteLine("gosh dang it, stem field money inc");
+                    break;
+                case "English":
+                    Console.WriteLine("useless subject ://");
+                    break;
+                case "Danish":
+                    Console.WriteLine("what are you, a dane? kek");
+                    break;
+                case "social studies":
+                    Console.WriteLine("time to take communications, and use it to start a blog etuu");
+                    break;
+                default:
+                    Console.WriteLine("pick between Danish,Math,Science,English and Social studies");
+                    break;
+            }
+        }
+    }
+}
